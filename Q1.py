733d66b489a9bb11ee552896b04f52fe4f3ee727
fbari <thefaisal.bari@gmail.com>
2023-06-24 10:23:32 -0400

    Q1, unfinished

--- Q1.py
+++ Q1.py
@@ -1,5 +1,5 @@
 # mlp classifier
-import numpy
+import numpy as np
 
 
 class InputLayer:
@@ -13,20 +13,44 @@ class HiddenLayer:
         self.Inputs = inputs
         self.NumberNodes = numberNodes
         self.Weights = weights
-        self.Outputs = None
+        self.Outputs = np.array([0, 0])
 
 
-def sigmoid(input):
-    return 1 / (1 + numpy.exp(-input))
+class OutputLayer:
+    def __init__(self, inputs, numberNodes, weights):
+        self.Inputs = inputs
+        self.NumberNodes = numberNodes
+        self.Weights = weights
+        self.Outputs = np.array([0])
 
 
-inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
-for input in inputs:
-    inp = InputLayer(input)
-    hid = HiddenLayer(inp.Outputs, 2, [-1, 1, 1])
+def Activation(input):
+    if input > 0:
+        return 1
+    else:
+        return 0
 
-    out = HiddenLayer(hid.Outputs, 1, [-1, 1, 1])
+
+def calculate(InputLayer, HiddenLayer):
+    return Activation(HiddenLayer.Weights[0] + HiddenLayer.Weights[1]*InputLayer.Outputs[0] +
+                      HiddenLayer.Weights[2]*InputLayer.Outputs[1])
 
 
 def calculateOutputs(inputs, HiddenLayer):
-    sigmoid(HiddenLayer.Weights[0] + HiddenLayer.Weights[1]*inputs[0] +)
+    return Activation(HiddenLayer.Weights[0] + HiddenLayer.Weights[1]*inputs[0] + HiddenLayer.Weights[2]*inputs[1])
+
+
+inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
+Weights = np.array([-1, 1, 1])
+def ANDgate(inputs, weights):
+    for input in inputs:
+        inp = InputLayer(input)
+        hid = HiddenLayer(inp.Outputs, 2, weights)
+        hid.Outputs[0] = calculate(inp, hid)
+        hid.Outputs[1] = calculate(inp, hid)
+
+        out = OutputLayer(hid.Outputs, 1, weights)
+        out.Outputs[0] = calculateOutputs(hid.Outputs, out)
+        print("The output for " + input + " is: " + out.Outputs[0])
+
+ANDgate(inputs, Weights)
